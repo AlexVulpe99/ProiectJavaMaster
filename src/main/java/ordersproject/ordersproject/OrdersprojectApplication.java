9b608061ac80faad4100eb92881ae499421767e4
AlexVulpe99 <44170240+AlexVulpe99@users.noreply.github.com>
2020-12-20 20:46:29 +0200

    Initial commit - project creation

---
+++ src/main/java/ordersproject/ordersproject/OrdersprojectApplication.java
@@ -0,0 +1,13 @@
+package ordersproject.ordersproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ordersproject/ordersproject/OrdersprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ordersproject.ordersproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
